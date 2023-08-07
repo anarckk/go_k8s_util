ee88d8030af3bd18db78da831ec7e552dd7d00e4
anarckk <anarckk@gmail.com>
2023-08-07 10:03:52 +0800

    1

--- go_k8s_util.go
+++ go_k8s_util.go
@@ -1,17 +1,23 @@
 package go_k8s_util
 
-import "gogs.bee.anarckk.me/anarckk/go_k8s_util/pod"
+import (
+	"gogs.bee.anarckk.me/anarckk/go_k8s_util/pod"
+	"gogs.bee.anarckk.me/anarckk/go_k8s_util/yaml"
+)
 
 type K8sUtil struct {
 	pod.PodUtil
+	yaml.YamlUtil
 }
 
 func NewK8s() (*K8sUtil, error) {
 	var k8sUtil K8sUtil
-	clientset, _, err := GetK8sClientset()
+	clientset, config, err := GetK8sClientset()
 	if err != nil {
 		return nil, err
 	}
+	k8sUtil.K8sConfig = config
+	k8sUtil.YamlUtil.ClientSet = clientset
 	k8sUtil.PodUtil.ClientSet = clientset
 	return &k8sUtil, nil
 }
